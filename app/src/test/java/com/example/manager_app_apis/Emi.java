c5a3a873c56050ca0346e843ea9381a776ad99e5
zubayersheba <zubayersheba18@gmail.com>
2020-10-31 11:18:07 +0600

    [Api automation with statuscode and response message]

--- app/src/test/java/com/example/manager_app_apis/Emi.java
+++ app/src/test/java/com/example/manager_app_apis/Emi.java
@@ -1,6 +1,34 @@
 package com.example.manager_app_apis;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class Emi {
+
+@Before
+    public void baseURL(){
+        RestAssured.baseURI="https://api.dev-sheba.xyz";
+    }
+    @Test
+    public void emiHome_response(){
+        when().get("v2/partners/37732/emi/home?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep").then()
+                .statusCode(200)
+                .body("code",equalTo(200))
+                .body("message",equalTo("Successful"));
+    }
+    @Test
+    public void emi_response(){
+        when().get("v2/partners/37732/emi?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep&recent=1").then()
+                .statusCode(200)
+                .body("code",equalTo(200))
+                .body("message",equalTo("Successful"));
+    }
+
     //https://api.dev-sheba.xyz/v2/partners/37732/emi/home?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep
     //https://api.dev-sheba.xyz/v2/partners/37732/emi?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep&recent=1
 }
--- app/src/test/java/com/example/manager_app_apis/e_shop.java
+++ app/src/test/java/com/example/manager_app_apis/e_shop.java
@@ -27,6 +27,14 @@ public class e_shop {
                 .body("code",equalTo(200))
                 .body("message",equalTo("Successful"));
     }
+
+    @Test
+    public void customerInfo_response(){
+        when().get("v2/partners/37732/my-customer-info?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep").then()
+                .statusCode(200)
+                .body("code",equalTo(200))
+                .body("message",equalTo("Successful"));
+    }
    /* https://api.dev-sheba.xyz/v2/services?is_business=1&lat=23.798260083979695&lng=90.40489386767149
     https://api.dev-sheba.xyz/v2/locations/current?lat=23.798260083979695&lng=90.40489386767149
     https://api.dev-sheba.xyz/v2/partners/37732/my-customer-info?remember_token=naQWZPwPCMQ7iuKL1pMQ7tsKlLMmF39f4XEiBwyF4AShSgvwejSjI0Wdo8ep
